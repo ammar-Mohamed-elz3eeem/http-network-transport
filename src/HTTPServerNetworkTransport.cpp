7ecd70e6ae50fa422e362a5ee93e00913dd96c6d
ammar-Mohamed-elz3eeem <ammarmohamedthez@gmail.com>
2024-09-15 02:46:52 +0300

    Add ConnectionAdapter class that inherits from HTTP::Connection class

--- src/HTTPServerNetworkTransport.cpp
+++ src/HTTPServerNetworkTransport.cpp
@@ -12,6 +12,125 @@
  */
 
 #include <HTTPNetworkTransport/HTTPServerNetworkTransport.hpp>
+#include <SystemAbstractions/NetworkEndpoint.hpp>
+
+namespace {
+	/**
+	 * @brief
+	 *     This class is an adapter between two related classes
+	 *     in different libraries
+	 *     - HTTP::Connection -- the interface required by HTTP
+	 *       library for sending & receiving data across the transport
+	 *       layer.
+	 *     - SystemAbstractions::NetworkConnection -- the class which
+	 *       implements a connection object in terms of the operating
+	 *       system's network APIs.
+	 */
+	struct ConnectionAdapter: public HTTP::Connection
+	{
+		// Properties
+
+		/**
+		 * @brief
+		 *     This is the object which is implementing the network
+		 *     connection in terms of the operating system's network APIs.
+		 */
+		std::shared_ptr<SystemAbstractions::NetworkConnection> adaptee;
+
+		/**
+		 * @brief
+		 *     This is the callback to call whenever data is received from
+		 *     the remote peer.
+		 */
+		DataReceivedDelegate dataReceivedDelegate;
+
+		/**
+		 * @brief
+		 *     This is the callback to call whenever the connection
+		 *     has been broken.
+		 */
+		BrokenDelegate brokenDelegate;
+
+		// Methods
+
+		/**
+		 * @brief
+		 *     This method should be called once the adapter is in place.
+		 *     it fires up the actual network processing.
+		 * 
+		 * @returns
+		 *     true if the adapter processing is successfully started,
+		 *     otherwise false is returned.
+		 */
+		bool wireUpAdapter() {
+			return adaptee->Process(
+				[this](const std::vector<uint8_t> &message)
+				{
+					if (dataReceivedDelegate != nullptr)
+						dataReceivedDelegate(message);
+				},
+				[this](bool graceful)
+				{
+					if (brokenDelegate != nullptr)
+						brokenDelegate();
+				}
+			);
+		}
+
+		// HTTP::Connection
+
+		/**
+		 * @brief
+		 *     This method sets the callback to call whenever data is received
+		 *     from the remote peer.
+		 * 
+		 * @param[in] delegate
+		 *     This is the callback to call whenever data is received from
+		 *     the remote peer.
+		 */
+		virtual void setDataReceivedDelegate(DataReceivedDelegate delegate) override {
+			dataReceivedDelegate = delegate;
+		};
+
+		/**
+		 * @brief
+		 *     This method sets the callback to call whenever the connection
+		 *     has been broken.
+		 * 
+		 * @param[in] delegate
+		 *     This is the callback to call whenever the connection
+		 *     has been broken.
+		 */
+		virtual void setBrokenDelegate(BrokenDelegate delegate) override {
+			brokenDelegate = delegate;
+		}
+
+		/**
+		 * @brief
+		 *     This method sends the given data to the remote peer.
+		 * 
+		 * @param[in] data
+		 *     This is the data to send to the remote peer.
+		 */
+		virtual void sendData(std::vector<uint8_t> data) override {
+			adaptee->SendMessage(data);
+		}
+
+		/**
+		 * @brief
+		 *     This method breaks the connection with the remote peer.
+		 * 
+		 * @param[in] clean
+		 *     indicates whether or not to complete any data transmission
+		 *     still in progress, before breaking the connection.
+		 */
+		virtual void breakConnection(bool clean) override {
+			// TODO: if clean == true, we need to hold off until adapter
+			// has written all its data.
+			adaptee->Close();
+		}
+	};
+}
 
 namespace HTTPNetworkTransport
 {
