e697a7853cba4cd7da646bac8e4269f05447cce6
ammar-Mohamed-elz3eeem <ammarmohamedthez@gmail.com>
2024-09-14 15:52:46 +0300

    Add HTTPNetworkTransport to test CMake + Add placeholder unit test

---
+++ test/src/HTTPServerNetworkTransportTests.cpp
@@ -0,0 +1,19 @@
+/**
+ * @file: HTTPNetworkTransport.cpp
+ * @author: Ammar Massoud (ammar@wpkama.com)
+ * 
+ * @brief
+ *     This file tests class HTTPNetworkTransport::HTTPNetworkTransport
+ * 
+ * @version: 1.0.0
+ * @date: 2024-07-28
+ * @copyright: Copyright (c) 2024
+ */
+
+#include <gtest/gtest.h>
+#include <HTTPNetworkTransport/HTTPServerNetworkTransport.hpp>
+
+TEST(HTTPServerNetworkTransportTests, Placeholder)
+{
+	ASSERT_TRUE(true);
+}
